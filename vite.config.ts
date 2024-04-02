5ad80cc5ac8e64395bfe4dad64f2eeaca3c5a950
Kevin Piacentini <kevinpiac@gmail.com>
2024-04-02 22:35:40 +0200

    🧑‍💻 Build with type definitions

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: { lib: {
-        entry: resolve(__dirname, 'src/main.ts'),
-            formats: ['es', 'cjs'],
-            fileName: 'index',
-    } },
-    resolve: { alias: { src: resolve('src/') } },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      formats: ['es', 'cjs'],
+      fileName: 'index'
+    }
+  },
+  plugins: [dts()],
+  resolve: { alias: { src: resolve('src/') } }
 })
